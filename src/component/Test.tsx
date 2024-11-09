f2a0493f2e85d1559cad36b9693648553a7c233c
zagaynov <zmp@digt.ru>
2024-11-09 22:28:03 +0300

    init commit

---
+++ src/component/Test.tsx
@@ -0,0 +1,14 @@
+import React, {FC} from 'react';
+import classes from './test.module.scss';
+
+
+const Test: FC = () => {
+  return (
+    <div className={classes.test}>
+      Test
+    </div>
+  );
+};
+
+export default Test;
+
